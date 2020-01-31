2d33d266de01b28167ca3a49b0142c1ad672a750
kentcheung2000 <kentcheung2000@yahoo.com>
2020-01-31 12:34:15 -0800

    Try to fix the mobile navigation

--- resources/js/script.js
+++ resources/js/script.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
     $('html, body').animate({ scrollTop: $('.js--section-features').offset().top }, 1000);
   });
 
-  // Add smooth scrolling to all links
+  // Add smooth scrolling to all links from one section to another section.
   $('a[href*="#"]').on('click', function(e) {
     e.preventDefault();
 
@@ -71,20 +71,61 @@ $(document).ready(function() {
     }
   );
 
-  /* Mobile nav */
-  $('.js--nav-icon').click(function() {
+  /*  Old Mobile nav */
+  $('.js--nav-icon, .js--main-nav a, .logo-black').click(function(element) {
     var nav = $('.js--main-nav');
     var icon = $('.js--nav-icon i');
 
-    nav.slideToggle(200);
-    if (icon.hasName('ion-navicon-round')) {
-      icon.addClass('close');
+    //Gets the class name of the element that triggered the event
+    var clicked = element.target.className;
+
+    //Exists the function if the menu is closed AND the logo-black element (logo image) was clicked
+    if (icon.hasClass('ion-navicon-round') && clicked == 'logo-black') return;
+
+    //Opens and closes the menu
+    if ($(window).width() < 768) {
+      nav.slideToggle(200);
+    }
+
+    //Changes icon states of the menu button
+    if (icon.hasClass('ion-navicon-round')) {
+      icon.addClass('ion-close-round');
       icon.removeClass('ion-navicon-round');
     } else {
       icon.addClass('ion-navicon-round');
-      icon.removeClass('close');
+      icon.removeClass('ion-close-round');
     }
   });
+
+  $(window).resize(function() {
+    var nav = $('.js--main-nav');
+    var icon = $('.js--nav-icon i');
+
+    if ($(window).width() > 767) {
+      nav.css('display', 'block');
+      icon.addClass('ion-close-round');
+      icon.removeClass('ion-navicon-round');
+    } else {
+      nav.css('display', 'none');
+      icon.addClass('ion-navicon-round');
+      icon.removeClass('ion-close-round');
+    }
+  });
+
+  // /*  Old Mobile nav */
+  // $('.js--nav-icon').click(function() {
+  //   var nav = $('.js--main-nav');
+  //   var icon = $('.js--nav-icon i');
+
+  //   nav.slideToggle(200);
+  //   if (icon.hasName('ion-navicon-round')) {
+  //     icon.addClass('close');
+  //     icon.removeClass('ion-navicon-round');
+  //   } else {
+  //     icon.addClass('ion-navicon-round');
+  //     icon.removeClass('close');
+  //   }
+  // });
 
   /* 
   var waypoints = $('#handler-first').waypoint(function(direction) {
